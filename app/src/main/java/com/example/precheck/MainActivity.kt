df20d30ca5acc3f5b28a25dd37ca209c938d1562
문몰리 <gsgslolo@naver.com>
2022-01-16 00:42:55 +0900

    :sparkles: init mainPage
    init mainPage

--- app/src/main/java/com/example/precheck/MainActivity.kt
+++ app/src/main/java/com/example/precheck/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.precheck
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.precheck.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        var binding = ActivityMainBinding.inflate(layoutInflater);
+        setContentView(binding.root);
+    }
+
+    fun tempFunction() {
+        var binding = ActivityMainBinding.inflate(layoutInflater);
+
     }
 }
