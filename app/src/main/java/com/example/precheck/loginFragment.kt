fcf99b9a8affc11302866570547a1244e8782e01
cpplovelove <gsgslolo@naver.com>
2022-01-17 22:54:36 +0900

    :sparkles: login & mainNote (1/3)
    login & mainNote (1/3)

--- app/src/main/java/com/example/precheck/loginFragment.kt
+++ app/src/main/java/com/example/precheck/loginFragment.kt
@@ -5,20 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.precheck.databinding.ActivityMainBinding
+import com.example.precheck.databinding.FragmentLoginBinding
+import kotlin.math.sign
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-class loginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+public class loginFragment : Fragment() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+        //로그인 버튼이 눌렸을 떄
+        var binding= FragmentLoginBinding.inflate(layoutInflater)
+        binding.signIn.setOnClickListener{
+            var id=binding.id.text;
+            var password =binding.password.text;
+            var mainActivity = activity as MainActivity;
+            mainActivity.getUserToken(id.toString(),password.toString());
         }
     }
 
@@ -26,18 +31,8 @@ class loginFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            loginFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
